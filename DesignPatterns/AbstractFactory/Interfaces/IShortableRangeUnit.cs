112a77736839db7edd68dfb65dacb3d499bf5d79
Henrique <trianonhenrique@gmail.com>
2019-07-12 15:35:05 -0300

    Added the basic structure and the editorconfig

---
+++ DesignPatterns/AbstractFactory/Interfaces/IShortableRangeUnit.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+    public interface IShortableRangeUnit
+    {
+        void Hit();
+    }
+}
