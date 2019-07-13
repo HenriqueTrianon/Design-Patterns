f243e727f9f1c1a1cb8c715abaf15d73ed3a60a8
Henrique <trianonhenrique@gmail.com>
2019-07-13 17:40:58 -0300

    Addition of some concretions

---
+++ DesignPatterns/AbstractFactory/Entities/Knight.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Entities
+{
+    public class Knight:IShortableRangeUnit
+    {
+        public void Hit()
+        {
+            
+        }
+    }
+}
---
+++ DesignPatterns/AbstractFactory/Entities/Samurai.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory.Entities
+{
+    public class Samurai:IShortableRangeUnit
+    {
+        public void Hit()
+        {
+            
+        }
+    }
+}
